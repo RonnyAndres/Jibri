62b4d8d1a4585d3431f86887dc448ca013640a0c
RonnyAndres <76404004+RonnyAndres@users.noreply.github.com>
2024-05-17 08:18:36 -0500

    Update previous_configuration.sh

--- previous_configuration.sh
+++ previous_configuration.sh
@@ -7,14 +7,20 @@ sudo apt-get update && sudo apt-get upgrade -y
 # Instalar wget
 sudo apt-get install wget -y
 
+# Abrir puertos en el firewall
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw allow 10000/udp
+sudo ufw reload
+
 # Descargar el instalador de Jitsi y Jibri
-wget -T 10 -O jitsi-jibri-installer https://raw.githubusercontent.com/jitsi-contrib/installers/main/jitsi-jibri/jitsi-jibri-installer
+wget -T 10 -O jitsi-jibri-installer https://raw.githubusercontent.com/jitsi-contrib/installers/main/jitsi-jibri-installer
 
 # Establecer variables de entorno
-export JITSI_HOST=jitsi.xoftix.com
-export TURN_HOST=turn.xoftix.com
-export JITSI_IP=10.0.2.15
-export DONT_SET_LETSENCRYPT=true
+export JITSI_HOST=www.zeroebs.com
+export TURN_HOST=turn.zeroebs.com
+export JITSI_IP=localhost
+export DONT_SET_LETSENCRYPT=false
 
 # Dar permisos de ejecución al instalador
 bash jitsi-jibri-installer
